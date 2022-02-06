77d4a27de78b43d97ac8fed3570ba736687d369d
sal infusino <sm121308@gmail.com>
2022-02-05 19:27:59 -0500

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWorksAPIDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public int Get (int id)
+        {
+            return id;
+        }
+
+        [HttpGet]
+        public string Test(int id)
+        {
+            string ret = String.Empty;
+
+            switch (id)
+            {
+                case 1:
+                    ret = "Suckcess";
+                    break;
+                case 2:
+                    ret = "Too";
+                    break;
+                case 3:
+                    ret = "Tree";
+                    break;
+                default:
+                    break;
+            }
+
+            return ret;
+        }
+    }
+}
